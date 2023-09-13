64561febf438c709215d00c4c8a267b40f8c52c9
Fatimah <aishafhonore@gmail.com>
2023-09-13 11:19:11 -0500

    Update New

--- views/New.jsx
+++ views/New.jsx
@@ -1,19 +1,28 @@
-const React = require("react");
-
 class New extends React.Component {
   render() {
+    const date = new Date();
+    const futureDate = date.getDate() + 367;
+    date.setDate(futureDate);
+    const defaultValue = date.toLocaleDateString("en-CA");
     return (
       <div>
-        <h1> New Flight Page </h1>
+        <h1>Add New Flight</h1>
         <form action="/flights" method="POST">
-          Airline: <input type="text" name="airline" /> <br />
-          FlightNo: <input type="number" name="flightNo" /> <br />
-          Departs: <input type="datetime-local" name="departs" /> <br />
-          <input type="submit" value="Create Flight" />
+          Airline <input type="text" name="airline" />
+          <br />
+          Flight Number{" "}
+          <input type="number" name="flightNo" min={10} max={9999} />
+          <br />
+          Departure{" "}
+          <input
+            type="datetime-locale"
+            name="departs"
+            defaultValue={defaultValue}
+          />
+          <br />
+          <input type="submit" value="Submit New Flight" />
         </form>
       </div>
     );
   }
 }
-
-module.exports = New;
